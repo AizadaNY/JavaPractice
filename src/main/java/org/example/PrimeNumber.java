d39573b2e0e4f72fe57a4aea2c2ce4981b9ad553
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-09-22 08:56:05 -0400

    PrimeNumber

--- src/main/java/org/example/PrimeNumber.java
+++ src/main/java/org/example/PrimeNumber.java
@@ -2,8 +2,18 @@ package org.example;
 
 public class PrimeNumber {
 
-    public static int primeNumber(int n) {
-        if ()
-            return 0;
+    public static boolean primeNumber(int n) {
+        for (int i = 2; i < 10; i++) {
+            if (n % i == 0 && n!=i) {
+                System.out.println(n%i);
+               return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(primeNumber(17));
     }
 }
